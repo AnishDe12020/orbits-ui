27293f21d69e8ec174b0f4a74014506bf8fe1fae
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-08-06 10:44:44 +0530

    chore(storybook): :bricks: added chakra storybook addon
    also changed storybook builder to webpack5 from vite

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -2,26 +2,19 @@ const path = require("path");
 
 module.exports = {
 	stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.tsx"],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@chakra-ui/storybook-addon"],
 	framework: "@storybook/react",
 	core: {
-		builder: "@storybook/builder-vite",
+		builder: "@storybook/builder-webpack5",
 	},
-	async viteFinal(config, { configType }) {
-		// customize the Vite config here
-		return {
-			...config,
-			resolve: {
-				alias: [
-					{
-						find: "orbi-chakra-theme",
-						replacement: path.resolve(
-							__dirname,
-							"../../../packages/orbi-chakra-theme/"
-						),
-					},
-				],
-			},
-		};
-	},
+	features: { emotionAlias: false },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+
+    return config
+  },
 };
