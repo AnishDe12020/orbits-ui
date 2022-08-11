5eaec631431f5934f3752e8a365ca386d659153b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-08-11 17:22:55 +0530

    feat(component): :lipstick: write some custom styles for the button component

--- apps/docs/stories/button.stories.tsx
+++ apps/docs/stories/button.stories.tsx
@@ -4,9 +4,28 @@ import { ComponentMeta } from "@storybook/react";
 
 import { Button } from "@chakra-ui/react";
 
-export const Default = () => <Button bg="brand.100">Text</Button>;
+export const Default = args => <Button {...args}>Default</Button>;
+Default.args = {
+  size: "md",
+  variant: "solid",
+};
 
 export default {
   title: "Button",
   component: Button,
+  argTypes: {
+    size: { control: { type: "select", options: ["sm", "md", "lg"] } },
+    variant: {
+      control: {
+        type: "select",
+        options: ["solid", "outline", "ghost", "link"],
+      },
+    },
+    colorScheme: {
+      control: {
+        type: "select",
+        options: ["cyan", "gray"],
+      },
+    },
+  },
 } as ComponentMeta<typeof Button>;
--- packages/orbi-chakra-theme/src/components/Button.ts
+++ packages/orbi-chakra-theme/src/components/Button.ts
@@ -1,10 +1,49 @@
-import type { ComponentStyleConfig } from "@chakra-ui/react";
+import type {
+  ComponentStyleConfig,
+  SystemStyleFunction,
+} from "@chakra-ui/react";
+
+import { mode } from "@chakra-ui/theme-tools";
+
+const variantSolid: SystemStyleFunction = props => {
+  const { colorScheme: c } = props;
+
+  const bg = mode(`${c}.200`, `${c}.700`)(props);
+
+  return {
+    bg,
+    color: mode(`gray.900`, `gray.100`)(props),
+    _hover: {
+      bg: mode(`${c}.300`, `${c}.800`)(props),
+      _disabled: {
+        bg,
+      },
+    },
+    _active: { bg: mode(`${c}.400`, `${c}.800`)(props) },
+  };
+};
 
 const Button: ComponentStyleConfig = {
-	baseStyle: {
-		borderRadius: "xl",
-		backgroundColor: "blue.500",
-	}
-}
+  baseStyle: { borderRadius: "lg" },
+  sizes: {
+    sm: {
+      fontSize: "sm",
+      px: "3",
+      py: "0.5",
+    },
+    md: {
+      fontSize: "md",
+      px: "4",
+      py: "1",
+    },
+    lg: {
+      fontSize: "lg",
+      px: "5",
+      py: "2",
+      borderRadius: "xl",
+    },
+  },
+  variants: { solid: variantSolid },
+};
 
 export default Button;
