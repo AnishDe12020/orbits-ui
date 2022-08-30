ed5397f11ec6bdfe2094d71da89c9d7e664d1c0d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-08-30 13:30:29 +0530

    fix(storybook): :bug: upgrade storybook to get rid of chakra ui color mode bugs
    also wip custom preview component (commented out for now)

---
+++ apps/docs/.storybook/preview.tsx
@@ -0,0 +1,75 @@
+import {
+  mainColors,
+  components,
+  accentBlue,
+} from "../../../packages/orbi-chakra-theme/src/index";
+
+// import * as React from "react";
+// import {
+//   ChakraProvider,
+//   ColorModeScript,
+//   extendTheme,
+//   Flex,
+//   IconButton,
+//   useColorMode,
+//   useColorModeValue,
+// } from "@chakra-ui/react";
+// import { Sun, Moon } from "react-feather";
+
+// const theme = extendTheme({
+//   components,
+//   semanticTokens: { colors: { ...mainColors, ...accentBlue } },
+//   styles: {
+//     global: {
+//       "html, body": {
+//         background: "brand.primary",
+//       },
+//     },
+//   },
+//   config: {
+//     initialColorMode: "dark",
+//     useSystemColorMode: false,
+//   },
+// });
+
+export const parameters = {
+  chakra: {
+    theme: {
+      components,
+      semanticTokens: { colors: { ...mainColors, ...accentBlue } },
+      styles: {
+        global: {
+          "html, body": {
+            background: "brand.primary",
+          },
+        },
+      },
+    },
+  },
+};
+
+// const withChakraUI = (StoryFn, context) => {
+//   const { toggleColorMode } = useColorMode();
+
+//   return (
+//     <>
+//       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
+//       <ChakraProvider theme={theme}>
+//         <Flex mb={8} justifyContent="end">
+//           <IconButton
+//             aria-label="Switch Color Mode"
+//             onClick={() => {
+//               console.log("t");
+//               toggleColorMode();
+//             }}
+//             icon={useColorModeValue(<Moon />, <Sun />)}
+//           />
+//         </Flex>
+//         <StoryFn {...context} />
+//       </ChakraProvider>
+//     </>
+//   );
+// };
+
+// export const decorators = [withChakraUI];
