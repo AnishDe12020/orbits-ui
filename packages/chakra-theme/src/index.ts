10a3876e74cce7c2303f0392e1c623def8ca87fc
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-09-07 16:03:54 +0530

    fix(chakra-theme): make chakra ui and react peer deps, add a default theme

--- packages/chakra-theme/src/index.ts
+++ packages/chakra-theme/src/index.ts
@@ -1,4 +1,36 @@
+import { ChakraTheme, extendTheme } from "@chakra-ui/react";
+
 export * from "./colors";
 export * from "./borders";
 
 export * from "./components";
+
+import {
+  mainColors,
+  accentBlue,
+  accentBlueTokens,
+  stateColors,
+} from "./colors";
+
+import { components } from "./components";
+
+const theme = extendTheme({
+  components,
+  colors: {
+    ...accentBlue,
+  },
+  semanticTokens: {
+    colors: { ...mainColors, ...accentBlueTokens, ...stateColors },
+  },
+  styles: {
+    global: {
+      "html, body": {
+        background: "brand.primary",
+      },
+    },
+  },
+}) as ChakraTheme;
+
+export { theme };
+
+export default theme;
